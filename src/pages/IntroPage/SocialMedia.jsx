a3837949466d7c44aa952c97b2ac7c51f05d7c38
RikuDev <riku@rikudev.com>
2023-05-01 20:56:17 +0200

    iconos svg azules en socialmedia, pequeño arreglo de height en contact page

--- src/pages/IntroPage/SocialMedia.jsx
+++ src/pages/IntroPage/SocialMedia.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import github from '../../assets/images/github.png'
-import twitter from '../../assets/images/twitter.png'
-import linkedin from '../../assets/images/linkedin.png'
+import githubBlue from '../../assets/images/githubBlue.svg'
+import twitter from '../../assets/images/twitter.svg'
+import linkedin from '../../assets/images/linkedin.svg'
 import styles from './SocialMedia.module.css'
 
 export function SocialMedia() {
 
     return (
         <div className={styles.SocialMedia}>
-        <a href='https://github.com/xRiiku' target='_blank' rel='noopener noreferrer'><img src={github} alt='Github logo'></img></a>
+        <a href='https://github.com/xRiiku' target='_blank' rel='noopener noreferrer'><img src={githubBlue} alt='Github logo'></img></a>
         <a href='https://www.linkedin.com/in/daniel-ram%C3%B3n-campos-0ab4b4254/' target='_blank' rel='noopener noreferrer'><img src={linkedin} alt='Linkedin logo'></img></a>
         <a href='https://twitter.com/Riku0h' target='_blank' rel='noopener noreferrer'><img src={twitter} alt='Twitter logo'></img></a>
         </div>
