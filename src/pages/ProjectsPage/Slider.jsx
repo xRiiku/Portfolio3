ecb9acee569ce79a7d72c36aa684c0fd4f928080
RikuDev <riku@rikudev.com>
2023-05-04 12:24:20 +0200

    añadido proyecto buscador perfiles github

--- src/pages/ProjectsPage/Slider.jsx
+++ src/pages/ProjectsPage/Slider.jsx
@@ -7,50 +7,64 @@ export function Slider() {
     const { t } = useTranslation()
 
     const slidesData = [
+
         {
             id: 1,
+            title: t('projects.pokedex'),
+            description: t('projects.pokedexDesc'),
+            imageUrl: 'https://www.rikudev.com/SliderImages/Pokedex.png',
+            link: 'https://www.rikudev.com/Pokedex/index.html'
+        },
+
+        {
+            id: 2,
+            title: t('projects.clone'),
+            description: t('projects.cloneDesc') ,
+            imageUrl: 'https://www.rikudev.com/SliderImages/Coinbase.png',
+            link: 'https://www.rikudev.com/Coinbase/html/proyecto.html'
+        },
+
+        {
+            id: 3,
+            title: t('projects.githubProfileSearch'),
+            description: t('projects.githubProfileSearchDesc'),
+            imageUrl: 'https://www.rikudev.com/SliderImages/githubProfileSearch.png',
+            link: 'https://www.rikudev.com/githubProfileSearch/index.html'
+        },
+
+        {
+            id: 4,
+            title: t('projects.calculator'),
+            description: t('projects.calculatorDesc'),
+            imageUrl: 'https://www.rikudev.com/SliderImages/Calculator.png',
+            link: 'https://rikudev.com/Calculator/index.html'
+        },
+
+        {
+            id: 5,
             title: t('projects.portfoliov1'),
             description: t('projects.portfoliov1Desc'),
             imageUrl: 'https://www.rikudev.com/SliderImages/PortfolioV1.png',
             link: 'https://www.rikudev.com/PortfolioV1/index.html'
         },
         {
-            id: 2,
+            id: 6,
             title: t('projects.portfoliov2'),
             description: t('projects.portfoliov2Desc'),
             imageUrl: 'https://www.rikudev.com/SliderImages/PortfolioV2.png',
             link: 'https://www.rikudev.com/PortfolioV2/index.html'
         },
+        
+        
         {
-            id: 3,
-            title: t('projects.clone'),
-            description: t('projects.cloneDesc') ,
-            imageUrl: 'https://www.rikudev.com/SliderImages/Coinbase.png',
-            link: 'https://www.rikudev.com/Coinbase/html/proyecto.html'
-        },
-        {
-            id: 4,
-            title: t('projects.pokedex'),
-            description: t('projects.pokedexDesc'),
-            imageUrl: 'https://www.rikudev.com/SliderImages/Pokedex.png',
-            link: 'https://www.rikudev.com/Pokedex/index.html'
-        },
-        {
-            id: 5,
-            title: t('projects.calculator'),
-            description: t('projects.calculatorDesc'),
-            imageUrl: 'https://www.rikudev.com/SliderImages/Calculator.png',
-            link: 'https://rikudev.com/Calculator/index.html'
-        },
-        {
-            id: 6,
+            id: 7,
             title: t('projects.puzzle'),
             description: t('projects.puzzleDesc'),
             imageUrl: 'https://www.rikudev.com/SliderImages/Puzzle.png',
             link: 'https://www.rikudev.com/Puzzle/index.html'
         },
         {
-            id: 7,
+            id: 8,
             title: t('projects.watch'),
             description: t('projects.watchDesc'),
             imageUrl: 'https://www.rikudev.com/SliderImages/watch.png',
